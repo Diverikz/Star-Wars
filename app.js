6acfa47e516302b754bc36b33c793b66a01733c7
Your Name <you@example.com>
2020-10-10 18:36:28 +0300

    modal-fix

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
-// Popup Function
-document.querySelector("#card").addEventListener("click", function () {
-  document.querySelector(".bg__popup").style.display = "flex";
-  document.body.style.overflow = "hidden";
-});
+$(function () {
+  const modalCall = $("[data-modal]");
+  const modalClose = $("[data-close]");
 
-document.querySelector(".close").addEventListener("click", function () {
-  document.querySelector(".bg__popup").style.display = "none";
-  document.body.style.overflow = "visible";
+  modalCall.on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalId = $this.data("modal");
+
+    $(modalId).addClass("show");
+    $("body").addClass("no-scroll");
+
+    setTimeout(function () {
+      $(modalId).find(".modal__dialog").css({
+        transform: "scale(1)",
+      });
+    }, 200);
+  });
+
+  modalClose.on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalParent = $this.parents(".modal");
+
+    modalParent.find(".modal__dialog").css({
+      transform: "scale(0)",
+    });
+
+    setTimeout(function () {
+      modalParent.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 200);
+  });
+
+  $(".modal").on("click", function (event) {
+    let $this = $(this);
+
+    $this.find(".modal__dialog").css({
+      transform: "scale(0)",
+    });
+
+    setTimeout(function () {
+      $this.removeClass("show");
+      $("body").removeClass("no-scroll");
+    }, 200);
+  });
+
+  $(".modal__dialog").on("click", function (event) {
+    event.stopPropagation();
+  });
 });
